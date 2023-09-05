3afc2549a48be7b3d5c2a4382a9b994396e8b837
WeakMap <105468126+1681202946450@users.noreply.github.com>
2023-09-05 16:23:29 +0800

    pic

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,18 @@
-export default function FirstPost() {
-    return <h1>First Post</h1>;
-  }
+import Image from 'next/image';
+import Link from 'next/link';
+
+const FirstPost = ()=> {
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+      <div>
+        <Image src="/manc.jpg" width="820" height="545"></Image>
+      </div>
+    </>
+  );
+}
+
+export default FirstPost

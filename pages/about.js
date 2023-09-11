12c9896376961c34f143f3b0046d650d3e8a18ba
WeakMap <105468126+1681202946450@users.noreply.github.com>
2023-09-12 01:17:59 +0800

    del: about.js

--- pages/about.js
+++
@@ -1,9 +0,0 @@
-//  pages/about.js
-export async function getStaticProps(context) {
-    return {
-      redirect: {
-        destination: '/',
-        permanent: true, // triggers 308
-      },
-    };
-  }

4cb8d210143d169e8d4a6b3cd32ca7c5ccdd79fd
WeakMap <105468126+1681202946450@users.noreply.github.com>
2023-09-12 01:12:21 +0800

    feat: end

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,21 +1,48 @@
 import Layout from '../../components/Layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
+import Date from '../../components/Data';
+import utilStyles from '../../styles/utils.module.css';
 
 
+// export default function Post({ postData }) {
+//     return <Layout>
+//         <Head>
+//             <title>{postData.title}</title>
+//         </Head>
+//         <Date dateString={postData.date} />
+
+//         {postData.title}
+//         <br />
+//         {postData.id}
+//         <br />
+//         {postData.date}
+//         <br />
+//         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+//     </Layout>
+// }
 export default function Post({ postData }) {
-    return <Layout>
-        {postData.title}
-        <br />
-        {postData.id}
-        <br />
-        {postData.date}
-    </Layout>
-}
+    return (
+      <Layout>
+        <Head>
+          <title>{postData.title}</title>
+        </Head>
+        <article>
+          <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+          <div className={utilStyles.lightText}>
+            <Date dateString={postData.date} />
+          </div>
+          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </article>
+      </Layout>
+    );
+  }
+  
 
 export async function getStaticPaths() {
     // Return a list of possible value for id
     const paths = getAllPostIds();
-    console.log('paths',paths)
+    console.log('paths', paths)
     return {
         paths,
         fallback: false,
@@ -23,7 +50,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-    const postData = getPostData(params.id);
+    const postData = await getPostData(params.id);
     return {
         props: {
             postData,
